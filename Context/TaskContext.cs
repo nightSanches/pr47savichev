49d8eb7fb8d9b83b75889dc349bee59d50181e72
nightSanches <galesavich@gmail.com>
2024-05-30 22:17:31 +0500

    Добавьте файлы проекта.

---
+++ Context/TaskContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using pr45savichev.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace pr45savichev.Context
+{
+    public class TaskContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+        public TaskContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql("server=127.0.0.1;port=3306;database=Base44;uid=root;pwd=", new MySqlServerVersion(new Version(8, 0, 11)));
+    }
+}
---
+++ Controllers/TasksController.cs
@@ -0,0 +1,62 @@
+﻿using pr45savichev.Context;
+using pr45savichev.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pr45savichev.Controllers
+{
+    [Route("api/TasksController")]
+    [ApiExplorerSettings(GroupName = "v1")]
+    public class TasksController : Controller
+    {
+        /// <summary>
+        /// Получение списка задач
+        /// </summary>
+        /// <remarks>Данный метод получает список задач, находящийся в базе данных</remarks>
+        /// <response code="200">Список успешно получен</response>
+        /// <response code="500">При выполнении запроса возникли ошибки</response>
+        [Route("List")]
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Tasks>), 200)]
+        [ProducesResponseType(500)]
+
+        public ActionResult List()
+        {
+            try
+            {
+                IEnumerable<Tasks> Tasks = new TaskContext().Tasks;
+                return Json(Tasks);
+            }
+            catch (Exception exp)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        /// <summary>
+        /// Получение задачи
+        /// </summary>
+        /// <remarks>Данный метод получает задачу, находящуюся в базе данных</remarks>
+        /// <response code="200">Задача успешно получена</response>
+        /// <response code="500">При выполнении запроса возникли ошибки</response>
+        [Route("Item")]
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Tasks>), 200)]
+        [ProducesResponseType(500)]
+        public ActionResult Item(int Id)
+        {
+            try
+            {
+                Tasks Task = new TaskContext().Tasks.Where(x => x.Id == Id).First();
+                return Json(Task);
+            }
+            catch (Exception exp)
+            {
+                return StatusCode(500);
+            }
+        }
+
+    }
+}
